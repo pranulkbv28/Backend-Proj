0e399a8f3d6b21592a124b9da641cf39956fe41d
pranulkbv28 <28bvpranaav@gmail.com>
2024-03-30 22:15:45 +0530

    config setup partially completed

---
+++ src/utils/cloudinary.js
@@ -0,0 +1,29 @@
+import { v2 as cloudinary } from "cloudinary";
+import fs from "fs";
+
+import { v2 as cloudinary } from "cloudinary";
+
+cloudinary.config({
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET,
+});
+
+const uploadOnCloudinary = async (localFilePath) => {
+  try {
+    if (!localFilePath) return "Error: No local file path found!!!!";
+
+    const response = await cloudinary.uploader.upload(localFilePath, {
+      resource_type: "auto",
+    });
+
+    console.log("File is uploaded on cloudinary successfully", response.url);
+
+    return response;
+  } catch (error) {
+    fs.unlinkSync(localFilePath); //remove the locallysaved temporary file as the upload operation to cloudinary failed
+    return null;
+  }
+};
+
+export { uploadOnCloudinary };
